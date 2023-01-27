6abc1f56ec220c602fae10c1bfd7fd0da334e891
Olasunbo Friday <wunmifyo@gmail.com>
2023-01-27 12:55:11 -0500

    My first commit

---
+++ src/introtojava/ArithmeticOperations.java
@@ -0,0 +1,17 @@
+package introtojava;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+int var1;
+var1 = 12;
+int var2;
+var2 = 38;
+int varTotal = var1 * var2;
+System.out.println(varTotal);
+		
+	}
+
+}
