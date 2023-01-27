90a495d9e554d530a69f6d83ba996cf00640bb34
Olasunbo Friday <wunmifryo@gmail.com>
2023-01-27 17:36:55 -0500

    My first commit

---
+++ src/introtojava/Hello.java
@@ -0,0 +1,11 @@
+package introtojava;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Olasunbo Friday");
+
+	}
+
+}
